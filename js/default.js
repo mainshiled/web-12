b35007f6b4f329aabc930f2c56fffb6b7156eb2f
mainshiled <yanakikouta1997@gmail.com>
2017-06-26 17:51:55 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,15 @@
+
+for (var n = 1; n < 101 ; n++ ){
+var li = docment.createElement('li');
+if(n/3==1||n/5==1){
+li.textContent='FizzBuss';
+}
+else if(n/3==1){
+li.text='Fizz';
+}
+else if(n/5==1){
+li.text='Buss'
+}
+else{
+li.text='n'
+}
