30b49c4755dd145ef358400b9d8cbc57bd253dff
mainshiled <yanakikouta1997@gmail.com>
2017-06-26 18:34:44 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 
 for (var i = 1; i < 101 ; i++ ){
-var li = docment.createElement('li');
+var li = document.createElement('li');
  
 if(i%3==0&&i%5==0){
 li.textContent='FizzBuss';
@@ -10,12 +10,12 @@ else if(n%3==0){
 li.textContent='Fizz';
 }
  
-else if(n%5==0){
+else if(i%5==0){
 li.textContent='Buss';
 }
  
 else{
-li.textContent='i';
+li.textContent=i;
 }
  
  document.getElementById('list').appendChild(li);
