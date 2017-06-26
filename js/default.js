cf163b36ca62c79e485e109ffa971cf175830e86
mainshiled <yanakikouta1997@gmail.com>
2017-06-26 18:52:56 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,12 +1,11 @@
-var number;
-for (var i = 1; i < 101^^i<number ; i++ ){
+for (var i = 1; i < 101 ; i++ ){
 var li = document.createElement('li');
  
 if(i%3==0&&i%5==0){
 li.textContent='FizzBuss';
 }
  
-else if(n%3==0){
+else if(i%3==0){
 li.textContent='Fizz';
 }
  
